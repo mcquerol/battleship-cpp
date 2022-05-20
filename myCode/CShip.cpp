a23f332714b2a06f6761e44c5b09e2587d5c767e
stmacebal <mateo.cebquerol@gmail.com>
2022-05-20 09:11:40 +0200

    Adding myCode\CShip.cpp on 2022-05-20

---
+++ myCode/CShip.cpp
@@ -0,0 +1,28 @@
+#include "CShip.h"
+#include "CGridPosition.h"
+
+/***************************************************************************
+*============= Copyright by Darmstadt University of Applied Sciences =======
+****************************************************************************
+* Filename        : CSHIP.CPP
+* Author          :
+* Description     :
+*
+*
+****************************************************************************/
+
+
+//System Include Files
+
+
+//Own Include Files
+
+//Method Implementations
+
+void CShip::CShip(){}
+void CShip::CShip(const CGridPosition& bow, const CGridPosition& stern){}
+bool CShip::isValid(){}
+CGridPosition CShip::getBow(){}
+CGridPosition CShip::getStern(){}
+ int CShip::length(){}
+ const std CShip::occupiedArea(){}
