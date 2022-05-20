25f80077892174a2e61b609bc1df3843b9c14676
stmacebal <mateo.cebquerol@gmail.com>
2022-05-20 09:11:38 +0200

    Adding myCode\CBoard.cpp on 2022-05-20

---
+++ myCode/CBoard.cpp
@@ -0,0 +1,22 @@
+#include "CBoard.h"
+
+/***************************************************************************
+*============= Copyright by Darmstadt University of Applied Sciences =======
+****************************************************************************
+* Filename        : CBOARD.CPP
+* Author          :
+* Description     :
+*
+*
+****************************************************************************/
+
+
+//System Include Files
+
+
+//Own Include Files
+
+//Method Implementations
+
+CBoard::CBoard(int rows, int columns){}
+void CBoard::operation1(){}
