b7ac179af27ebc9e071bbcf9d38c0f2549a8e745
stmacebal <mateo.cebquerol@gmail.com>
2022-05-20 09:11:38 +0200

    Adding myCode\CGridPosition.cpp on 2022-05-20

---
+++ myCode/CGridPosition.cpp
@@ -0,0 +1,29 @@
+#include "CGridPosition.h"
+#include <iostream>
+
+using namespace std;
+/***************************************************************************
+*============= Copyright by Darmstadt University of Applied Sciences =======
+****************************************************************************
+* Filename        : CGRIDPOSITION.CPP
+* Author          :
+* Description     :
+*
+*
+****************************************************************************/
+
+
+//System Include Files
+
+
+//Own Include Files
+
+//Method Implementations
+
+void CGridPosition::CGridPosition(char row, int column){}
+void CGridPosition::CGridPosition(std::string position){}
+bool CGridPosition::isValid(){return true;}
+char CGridPosition::getRow(){return 'a';}
+int CGridPosition::getColumn(){return 0;}
+//void CGridPosition::operation1(){}
+//void CGridPosition::operator==(GridPosition){}
