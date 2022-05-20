f855d7a8c667f4867d65d78edf1ac83e4636598c
stmacebal <mateo.cebquerol@gmail.com>
2022-05-20 09:11:39 +0200

    Adding myCode\COpponentGrid.cpp on 2022-05-20

---
+++ myCode/COpponentGrid.cpp
@@ -0,0 +1,23 @@
+#include "COpponentGrid.h"
+
+/***************************************************************************
+*============= Copyright by Darmstadt University of Applied Sciences =======
+****************************************************************************
+* Filename        : COPPONENTGRID.CPP
+* Author          :
+* Description     :
+*
+*
+****************************************************************************/
+
+
+//System Include Files
+
+
+//Own Include Files
+
+//Method Implementations
+
+void COpponentGrid::COpponentGrid(int rows, int columns){}
+ int COpponentGrid::getRows(){}
+ int COpponentGrid::getColumns(){}
