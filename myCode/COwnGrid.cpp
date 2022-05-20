5f2a96f7b07601dadf1b77764aceef08181c50d3
stmacebal <mateo.cebquerol@gmail.com>
2022-05-20 09:11:40 +0200

    Adding myCode\COwnGrid.cpp on 2022-05-20

---
+++ myCode/COwnGrid.cpp
@@ -0,0 +1,36 @@
+#include "COwnGrid.h"
+#include <iostream>
+
+using namespace std;
+/***************************************************************************
+*============= Copyright by Darmstadt University of Applied Sciences =======
+****************************************************************************
+* Filename        : COWNGRID.CPP
+* Author          :
+* Description     :
+*
+*
+****************************************************************************/
+
+
+//System Include Files
+
+
+//Own Include Files
+
+//Method Implementations
+
+void COwnGrid::COwnGrid(int rows, int columns)
+{
+
+}
+int COwnGrid::getRows()
+{
+	return 0;
+}
+int COwnGrid::getColumns()
+{
+return 0;
+}
+bool COwnGrid::placeShip(const CShip& ship){return true;}
+const vector COwnGrid::getShips(){}
