0e80d57f810115205f04dd23c99741746e482eb1
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2024-01-03 13:51:53 +0100

    implemented methods

--- myCode/CGridPosition.cpp
+++ myCode/CGridPosition.cpp
@@ -5,74 +5,35 @@
 using namespace std;
 
 
-CGridPosition::CGridPosition()
-{
-
-}
-
 /** CGridPosition constructor 1
  *
  * this constructor takes individual char and int parameters for the
  * row and column respecitvely
  */
-CGridPosition::CGridPosition(char row, int column)
-{
-	this->row = row;
-	this->column = column;
-}
+CGridPosition::CGridPosition(char row, int column) : row(row), column(column) {}
 
 /** CGridPosition contstructor 2
  *
  * this constructor takes a string input (eg. "B10") and splits it
  * into the row and column
  */
-CGridPosition::CGridPosition(std::string position)
-{
-	//TODO make sure it can take 10 as a result
-	for(unsigned int i = 0; i < position.length(); i++)
-	{
-		if(isdigit(position[i]))
-		{
-			if(position[i] == '1' && position[i+1] == '0')
-			{
-				this->column = 10;
-			}
-			else
-			{
-				char pos = position[i];
-				this->column = std::stoi(&pos);
-			}
-		}
-		else
-		{
-			this->row = position[i];
-		}
-	}
-}
+CGridPosition::CGridPosition(std::string position) : row(position[0]), column(std::stoi(position.substr(1))) {}
 
 /** CGridPosition::isValid
  *
  * checks if the
  * row is between 'A' and 'Z' and the column is greater than 1
  */
-bool CGridPosition::isValid()
+bool CGridPosition::isValid() const
 {
-	if(((int)row >= 65 && (int)row <= 90) && column >= 1)
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
-
+	return (row >= 'A' && row <= 'J') && (column >= 1);
 }
 
 /** CGridPosition::getRow
  *
  * return the row
  */
-char CGridPosition::getRow()
+char CGridPosition::getRow() const
 {
 	return this->row;
 }
@@ -81,7 +42,7 @@ char CGridPosition::getRow()
  *
  * return the column
  */
-int CGridPosition::getColumn()
+int CGridPosition::getColumn() const
 {
 	return this->column;
 }
@@ -106,11 +67,7 @@ CGridPosition::operator std::string() const
  */
 bool CGridPosition::operator<(CGridPosition other) const
 {
-	if((int)this->row < (int)other.row || this->column < other.column)
-	{
-		return true;
-	}
-	return false;
+	return (row < other.row) || ((row == other.row) && (column < other.column));
 }
 
 /** '==' operator overload
@@ -120,9 +77,5 @@ bool CGridPosition::operator<(CGridPosition other) const
  */
 bool CGridPosition::operator==(CGridPosition other) const
 {
-	if((int)this->row == (int)other.row && this->column == other.column)
-	{
-		return true;
-	}
-	return false;
+	return (row == other.row) && (column == other.column);
 }
