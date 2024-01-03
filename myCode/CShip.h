6c9666ce7fad7616d302f64c5cc9440412018f58
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2024-01-03 14:43:53 +0100

    fixed declarations

--- myCode/CShip.h
+++ myCode/CShip.h
@@ -6,6 +6,8 @@
 #include <iostream>
 #include <set>
 #include <string>
+#include <iterator>
+#include <set>
 
 class CShip {
 private:
@@ -26,32 +28,32 @@ public:
      * checks if the
      * bow and stern are valid cooridinates for the 10x10 board
      */
-    bool isValid();
+    bool isValid() const;
 
     /** CShip::getBow
      *
      * return the bow
      */
-    CGridPosition getBow();
+    CGridPosition getBow() const;
 
     /** CShip::getStern
      *
      * return the stern
      */
-    CGridPosition getStern();
+    CGridPosition getStern() const;
 
     /** CShip::length
      *
      * return the length of the ship
      */
-    int length();
+    int length() const;
 
     /** CShip::occupiedArea
      *
      * returns the size of a set of coordinates which is the area of the board covered
      * by a ship. This is calculated based upon the bow and stern
      */
-    const std::set<CGridPosition>occupiedArea();
+    const std::set<CGridPosition> occupiedArea() const;
 
     /** CShip::blockedArea
      *
@@ -59,6 +61,6 @@ public:
      * by a ship but ships cannot touch one another so a minimum of one space needs to be kept between each ship
      * this area also includes the area of the ship
      */
-    //const std::set<CGridPosition>blockedArea();
+    const std::set<CGridPosition> blockedArea() const;
 };
 #endif /* CSHIP_H */
