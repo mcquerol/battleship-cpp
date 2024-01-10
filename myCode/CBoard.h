e3f1d011a8c03b0ddf45a52e70a488bf2eeab779
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2024-01-10 21:41:02 +0000

    added myCode\CBoard.h

---
+++ myCode/CBoard.h
@@ -0,0 +1,53 @@
+#ifndef CBOARD_H
+#define CBOARD_H
+
+#include "COpponentGrid.h"
+#include "COwnGrid.h"
+
+class CBoard {
+private:
+	int rows;
+	int columns;
+
+	COwnGrid ownGrid;
+
+    COpponentGrid opponentGrid;
+
+
+public:
+
+
+    /** CBoard constructor
+     *
+     * takes in rows and columns as parameters
+     * ownGrid object and opponentGrid objects
+     * are created with those dimensions
+     */
+    CBoard(int rows, int columns);
+
+    /** CBoard::getRows
+     *
+     * return the rows
+     */
+    int getRows() const;
+
+    /** CBoard::getColumns
+     *
+     * return the columns
+     */
+    int getColumns() const;
+
+    /** CBoard::getOwnGrid
+     *
+     * returns a reference of the ownGrid object
+     */
+    COwnGrid& getOwnGrid();
+
+    /** CBoard::opponentGrid
+     *
+     * returns a reference of the opponentGrid object
+     */
+    COpponentGrid& getOpponentGrid();
+};
+
+#endif /* CBOARD_H */
