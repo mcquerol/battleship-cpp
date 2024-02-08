8b9910585ef9d7495c9652d47591ae50fb8c4c80
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2024-02-08 03:23:18 +0100

    implemented test cases with various ship locations

--- myCode/main.cpp
+++ myCode/main.cpp
@@ -9,48 +9,44 @@
 #include "CConsoleView.h"
 #include <vector>
 #include <set>
+
+#include "part1tests.h"
+#include "part2tests.h"
+#include "part3tests.h"
+
 using namespace std;
 
 int main() {
-    CBoard b(10, 10);
 
-    CGridPosition p1('B', 2);
-    CGridPosition p2('B', 4);
-    CShip ship1(p1, p2);
+//	part1tests test1;
+//    test1.part1test();
+//
+//	part1tests test2;
+//    test2.part1test();
+//
+//	part1tests test3;
+//    test3.part1test();
+	CBoard b(10, 10);
 
-    CGridPosition p3('D', 4);
-    CGridPosition p4('G', 4);
-    CShip ship2(p3, p4);
+	CGridPosition p1('B', 2);
+	CGridPosition p2('B', 4);
+	CShip ship1(p1, p2);
 
-    CGridPosition p5('F', 6);
-    CGridPosition p6('F', 9);
-    CShip ship3(p5, p6);
+	CGridPosition p3('D', 4);
+	CGridPosition p4('G', 4);
+	CShip ship2(p3, p4);
 
-    b.getOwnGrid().placeShip(ship1);
-    b.getOwnGrid().placeShip(ship2);
-    b.getOwnGrid().placeShip(ship3);
+	CGridPosition p5('F', 6);
+	CGridPosition p6('F', 9);
+	CShip ship3(p5, p6);
 
-    CConsoleView view(&b);
-    view.print();
+	b.getOwnGrid().placeShip(ship1);
+	b.getOwnGrid().placeShip(ship2);
+	b.getOwnGrid().placeShip(ship3);
 
+	CConsoleView view(&b);
+	view.print();
 
-    const std::vector<CShip> ships = b.getOwnGrid().getShips();
-
-    // Print information about each ship
-    for (const auto& ship : ships) {
-
-        // Get the occupied area of the ship
-        const std::set<CGridPosition>& occupiedArea = ship.occupiedArea();
-
-        // Print the positions of the ship
-        std::cout << "Occupied Area: " << endl;
-        for (const auto& position : occupiedArea) {
-            std::cout << position.getRow() << "" << position.getColumn() << endl;
-        }
-        cout << ship.length() << endl;
-        std::cout << std::endl;
-
-    }
     return 0;
 }
 
